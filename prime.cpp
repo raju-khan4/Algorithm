0aa2aab32d11516917a9ab6f91525f3619b8cdac
raju-khan4 <mdrajukhan8756@gmail.com>
2025-11-11 21:49:32 +0600

    Add prime number checker in prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i;
+    bool isPrime = true;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n <= 1) {
+        isPrime = false;  // 0 and 1 are not prime
+    } else {
+        // check from 2 to n-1
+        for (i = 2; i <= n/2; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;  // not prime
+            }
+        }
+    }
+
+    if (isPrime) {
+        cout << n << " is a prime number." << endl;
+    } else {
+        cout << n << " is not a prime number." << endl;
+    }
+
+    return 0;
+}
